5851d374fb5292a4ae15ac597c64c089ade8b95e
ChaitanyaChaitu <141060156+chaitanya1330@users.noreply.github.com>
2024-01-05 23:16:01 +0530

    Update settings.py

--- new_chatapp/fundoo/fundoo/settings.py
+++ new_chatapp/fundoo/fundoo/settings.py
@@ -104,12 +104,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'chatapp_db',
-        'USER' : os.environ.get('DATABASE_USER'),
-        'PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
+        # 'USER' : os.environ.get('DATABASE_USER'),
+        # 'PASSWORD' : os.environ.get('DATABASE_PASSWORD'),
         # 'HOST' : os.environ.get('DATABASE_HOST'),
         # 'PORT' : os.environ.get('DATABASE_PORT'),
-        #'USER': 'chay',
-        #'PASSWORD': 'chay@1234',
+        'USER': 'chay',
+        'PASSWORD': 'chay@1234',
         'HOST': 'custom-con', 
         'PORT': '3306',
     }
